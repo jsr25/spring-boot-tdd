532a1de034c437dc4cd82d93d799dd396193c975
jsr25 <ramosjuans@live.com.ar>
2021-09-15 23:26:47 -0500

    create test to enpoint to create

--- src/test/java/org/adaschool/tdd/WeatherReportControllerTest.java
+++ src/test/java/org/adaschool/tdd/WeatherReportControllerTest.java
@@ -8,11 +8,10 @@ import org.adaschool.tdd.service.MongoWeatherService;
 import org.adaschool.tdd.service.WeatherService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
@@ -27,36 +26,35 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest( webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT )
-public class WeatherControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WeatherReportControllerTest {
+
+    WeatherService weatherService;
+
+    @MockBean
+    WeatherReportRepository repository;
 
     @LocalServerPort
     private int port;
 
-    WeatherService weatherService;
-
-    @Mock
-    WeatherReportRepository repository;
-
-    @BeforeEach()
-    public void setup()
-    {
-        weatherService = new MongoWeatherService( repository );
-    }
-
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @BeforeEach()
+    public void setup() {
+        weatherService = new MongoWeatherService(repository);
+    }
+
     @Test
-    void weatherCreate() throws Exception{
+    void weatherCreate() throws Exception {
         double lat = 4.7110;
         double lng = 74.0721;
-        GeoLocation location = new GeoLocation( lat, lng );
-        WeatherReportDto weatherReportDto = new WeatherReportDto( location, 35f, 22f, "tester", new Date() );
+        GeoLocation location = new GeoLocation(lat, lng);
+        WeatherReportDto weatherReportDto = new WeatherReportDto(location, 35f, 22f, "tester", new Date());
         WeatherReport weatherReport = new WeatherReport(weatherReportDto);
         when(repository.save(any(WeatherReport.class))).thenReturn(weatherReport);
 
-        final String baseUrl = "http://localhost:"+port+"/v1/weather";
+        final String baseUrl = "http://localhost:" + port + "/v1/weather";
         URI uri = new URI(baseUrl);
 
         HttpHeaders headers = new HttpHeaders();
